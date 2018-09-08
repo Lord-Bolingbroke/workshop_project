df1e65c8ce930bba46bfdaf14cadaf0b988ccf65
Noah_Schooler <noahschooler1@gmail.com>
2018-09-08 14:10:11 -0700

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#goal-button").click(function() {
+      $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+      $("#new-goal").val("");
+    })
+})
+alert("Hello World!");
+alert(2 + 2);
+alert(4 * "2");
+alert(8 + "2");
+alert(10-2);
+alert(10/2);
